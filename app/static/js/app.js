835fe7a9aa43499e95508c93c4365974d9953a8d
Jordan Lewis <jordankareemlewis@gmail.com>
2021-04-04 19:33:27 -0500

    removed api token

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,11 +1,100 @@
 /* Add your Application JavaScript */
+const Home = {
+  name: 'Home',
+  template: `
+  <div class="home">
+  <img src="/static/images/logo.png" alt="VueJS Logo">
+  <h1>{{ welcome }}</h1>
+  </div>
+  `,
+  data() {
+  return {
+  welcome: 'Hello World! Welcome to VueJS'
+  }
+  }
+  };
+
+  const NewsList ={
+    name: 'NewsList',
+    template: `
+    <div class="form-inline d-flex justify-content-center">
+  <div class="form-group mx-sm-3 mb-2">
+  <label class="sr-only" for="search">Search</label>
+  <input type="search" name="search" v-model="searchTerm"
+  id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+  <button class="btn btn-primary mb-2"
+  @click="searchNews">Search</button>
+  </div>
+  </div>
+    <div class="news">
+        <h2 class="text-center">News</h2>
+        <ul class="news__list">
+        <li v-for="article in articles"
+        class="news__item">{{ article.title }} <img :src="article.urlToImage" alt="no image available">{{ article.description }} </li>
+        </ul>
+    </div>     
+    `,
+    created() {
+      let self = this;
+      fetch('https://newsapi.org/v2/top-headlines?country=us',
+      {
+      headers: {
+      'Authorization': 'Bearer <api token>'
+      }
+      })
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(data) {
+      console.log(data);
+      self.articles = data.articles;
+      });
+      },
+        data() {
+          return {
+          articles: [],
+          searchTerm: ''
+          }
+          },
+          methods: {
+            searchNews() {
+            let self = this;
+            fetch('https://newsapi.org/v2/everything?q='+
+            self.searchTerm + '&language=en', {
+            headers: {
+            'Authorization': 'Bearer <api token>'
+            }
+            })
+            .then(function(response) {
+            return response.json();
+            })
+            .then(function(data) {
+            console.log(data);
+            self.articles = data.articles;
+            });
+            }
+            }
+      }
+  
 const app = Vue.createApp({
   data() {
     return {
-      welcome: 'Hello World! Welcome to VueJS'
     }
-  }
+  },
+  components: {
+    'home': Home,
+    'news-list': NewsList
+    }
 });
+
+const router = VueRouter.createRouter({
+  history: VueRouter.createWebHistory(),
+  routes: [
+  { path: '/', component: Home },
+  { path: '/news', component: NewsList }
+  ]
+  });
+
 
 app.component('app-header', {
   name: 'AppHeader',
@@ -20,10 +109,10 @@ app.component('app-header', {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <router-link to="/" class="nav-link">Home</router-link>
                 </li>
                 <li class="nav-item">
-                  <a class="nav-link" href="#">News</a>
+                <router-link to="/news" class="nav-link">News</router-link>
                 </li>
               </ul>
             </div>
@@ -34,6 +123,7 @@ app.component('app-header', {
     return {};
   }
 });
+
 
 app.component('app-footer', {
   name: 'AppFooter',
@@ -51,4 +141,5 @@ app.component('app-footer', {
   }
 })
 
+app.use(router)  
 app.mount('#app');
